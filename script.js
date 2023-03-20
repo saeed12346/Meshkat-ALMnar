e4a328a070eaa32add9fea9098ed22c7a5e4ec40
saeed12346 <89911736+saeed12346@users.noreply.github.com>
2023-03-21 02:48:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const mobileToggle = document.getElementById("mobile-toggle");
+
+mobileToggle.addEventListener("click", () => {
+  document.querySelector(".nav-items").classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".nav-item").forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    document.querySelector(".nav-items").classList.toggle("active");
+  });
+});
+
+
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = '';
+        body.style.feature = 'white';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'gray';
+        body.style.color = 'gray';
+        body.style.transition = '2s';
+    }
+});
