77b0465292d32735d489a02f84ddc060bba68449
saeed12346 <89911736+saeed12346@users.noreply.github.com>
2023-03-26 06:09:59 +0300

    update-slider

--- script.js
+++ script.js
@@ -11,18 +11,41 @@ const navLinks = document.querySelectorAll(".nav-item").forEach((navItem) => {
 });
 
 
-const toggle = document.getElementById('toggleDark');
-const body = document.querySelector('body');
 
-toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
-        body.style.background = '';
-        body.style.feature = 'white';
-        body.style.transition = '2s';
-    }else{
-        body.style.background = 'gray';
-        body.style.color = 'gray';
-        body.style.transition = '2s';
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-});
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+// Auto Slide   if you need auto slide ,remove the commit "//"
+//var slideIndex = 0;
+//showSlides();
+//function showSlides() {
+    //var i;
+  //  var slides = document.getElementsByClassName("mySlides");
+   // for (i = 0; i < slides.length; i++) {
+   //     slides[i].style.display = "none";
+   // }
+  //  slideIndex++;
+   // if (slideIndex > slides.length) { slideIndex = 1 }
+   // slides[slideIndex - 1].style.display = "block";
+   // setTimeout(showSlides, 2000); // Change image every 2 seconds
+//}
